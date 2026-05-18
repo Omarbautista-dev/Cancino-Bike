a278c39867d21e0d3d07bf2e4ab9c0722e763fff
Omarbautista-dev <omardejesusb59@gmail.com>
2026-05-18 14:37:57 -0600

    Implementacion de usuario para la bd

--- src/main/java/org/example/database/ConexionBD.java
+++ src/main/java/org/example/database/ConexionBD.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/sig_cb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String USER = "sigcb_app";
+    private static final String PASSWORD = "SigCB_2026*";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
